62de286ba581a97a6e78f38f1b53ce7fad4f8573
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-03-29 16:41:00 -0300

    Initialize chessboard with piece placements

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 tabuleiro = [["   " for j in range(8)]for i in range(8)]
+pecas_brancas = [' T ',' C ','>B ',' D ',' R ',' B<',' C ',' T ',' P ']
+pecas_pretas = [' t ',' c ','>b ',' d ',' r ',' b<',' c ',' t ',' p ']
 
 for z in range(8):
     for n in range(8):
+        if z == 0:
+            tabuleiro[z][n] = pecas_brancas[n]
+        elif z == 1:
+            tabuleiro[z][n] = pecas_brancas[8]
+
+        if z == 6:
+            tabuleiro[z][n] = pecas_pretas[8]
+        elif z == 7:
+            tabuleiro[z][n] = pecas_pretas[n]
         print(f"[{tabuleiro[z][n]}]", end="")
 
     print()
