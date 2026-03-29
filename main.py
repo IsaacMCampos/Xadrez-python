36722143628350aa8180e4fd627efc6b61327963
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-03-29 16:51:10 -0300

    Create montar_tabuleiro function for board setup
    Refactored board setup into a function for clarity.

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ tabuleiro = [["   " for j in range(8)]for i in range(8)]
 pecas_brancas = [' T ',' C ','>B ',' D ',' R ',' B<',' C ',' T ',' P ']
 pecas_pretas = [' t ',' c ','>b ',' d ',' r ',' b<',' c ',' t ',' p ']
 
-for z in range(8):
-    for n in range(8):
-        if z == 0:
-            tabuleiro[z][n] = pecas_brancas[n]
-        elif z == 1:
-            tabuleiro[z][n] = pecas_brancas[8]
+def montar_tabuleiro():
+    for l in range(8):
+        for c in range(8):
+            if l == 0:
+                tabuleiro[l][c] = pecas_brancas[c]
+            elif l == 1:
+                tabuleiro[l][c] = pecas_brancas[8]
 
-        if z == 6:
-            tabuleiro[z][n] = pecas_pretas[8]
-        elif z == 7:
-            tabuleiro[z][n] = pecas_pretas[n]
-        print(f"[{tabuleiro[z][n]}]", end="")
+            if l == 6:
+                tabuleiro[l][c] = pecas_pretas[8]
+            elif l == 7:
+                tabuleiro[l][c] = pecas_pretas[c]
+            print(f"[{tabuleiro[l][c]}]", end="")
+        print()
 
-    print()
+montar_tabuleiro()
